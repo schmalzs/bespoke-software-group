f698f6a080f7c718ad159cb232a59e7826cbf045
Scott Schmalz <scott.schmalz@gmail.com>
2022-03-12 19:05:51 -0500

    fix svg in header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,23 +4,25 @@ import Logo from './Logo';
 export default function Header() {
   return (
     <>
-      <header className="flex items-center bg-gray font-extrabold text-white text-xl md:text-4xl p-4">
-        <Link className="mr-4" to="/">
-          <Logo className="fill-transparent stroke-react w-8 h-8 md:w-14 md:h-14" />
-        </Link>
-        {<h1>Bespoke Software Group</h1>}
+      <header className="flex flex-col bg-gray font-extrabold text-white text-xl md:text-4xl">
+        <div className="flex items-center p-4">
+          <Link className="mr-4" to="/">
+            <Logo className="fill-transparent stroke-react w-8 h-8 md:w-14 md:h-14" />
+          </Link>
+          <h1>Bespoke Software Group</h1>
+        </div>
+        <svg
+          className="bg-react h-4 md:h-8 w-full"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+          version="1.1"
+          viewBox="0 0 2560 100"
+          x="0"
+          y="0"
+        >
+          <polygon className="fill-gray" points="0 0 2560 100 2560 0"></polygon>
+        </svg>
       </header>
-      <svg
-        className="bg-react h-4 md:h-8 w-full"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-        version="1.1"
-        viewBox="0 0 2560 100"
-        x="0"
-        y="0"
-      >
-        <polygon className="fill-gray" points="0 0 2560 100 2560 0"></polygon>
-      </svg>
     </>
   );
 }
