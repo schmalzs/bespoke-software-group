81cea410af27ba7be0ceb4239a7e715a123ad88f
Scott Schmalz <scott.schmalz@gmail.com>
2022-03-26 16:12:14 -0400

    Tweak footer content

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="mt-2 p-2 text-xs">
-      &copy; {new Date().getFullYear()} Bespoke Software Group, LLC
+      &copy; {new Date().getFullYear()} Bespoke Software Group LLC
     </footer>
   );
 }
